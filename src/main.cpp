4b9c077ec4d50cfecaab631bbae110d69c1de4ef
Maksim Kulis <maksimkulis@yandex.ru>
2020-11-03 20:47:41 +0300

    change mkdir lib

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,17 @@
-#include <sys/stat.h>
+#include <filesystem>
+
 #include "balk.h"
 #include "parser.h"
 #include "lu_solver.h"
+
+namespace fs = std::filesystem;
 
 int main(int argc, char **argv)
 {
     Parser pars;
     Balk balk = pars.get_balk_from_file(argv[1]);
     // create folder "outputs" for saving results of the solve_balk();
-    mkdir("./outputs", 0755);
+    fs::create_directory("outputs");
     balk.solve_balk();
     return 0;
 }
